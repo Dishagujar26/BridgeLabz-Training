ec5180444ebedbe09c73805cb399130bba29d7ea
Dishagujar26 <dishagujar1@gmail.com>
2025-12-19 13:06:35 +0530

    [Disha Gujar] : Add bonus calculation program

---
+++ gcr-codebase/control-flow/Level-two/BonusCalculation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Create BonusCalculation Class to compute bonus of employees
+
+public class BonusCalculation{  
+	public static void main(String []args){
+	    
+		// Create a Scanner object 
+        Scanner sc = new Scanner(System.in);
+		
+        // Get input value of salary        		
+		double salary = sc.nextDouble();
+		
+        // Get input value of year    		
+		int year = sc.nextInt();
+		
+	    double bonus = 0.0;
+		
+		// Calculate bonus amount 
+        if(year > 5){
+            bonus = salary * 1.05;
+        }    		
+		
+		System.out.println("Bonus is " + bonus);
+		
+		// Closing the Scanner Stream
+		sc.close();
+         		
+    }
+}
