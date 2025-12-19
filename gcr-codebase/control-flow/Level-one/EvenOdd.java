699fd75e72ecc8ed5cbef468b816668bcb51bb20
Dishagujar26 <dishagujar1@gmail.com>
2025-12-19 12:51:39 +0530

    [Disha Gujar] : Add even odd status of a number program

---
+++ gcr-codebase/control-flow/Level-one/EvenOdd.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Create EvenOdd Class to print odd and even numbers 
+
+public class EvenOdd{  
+	public static void main(String []args){
+	    
+		// Create a Scanner object 
+        Scanner sc = new Scanner(System.in);
+		
+        // Get input value of number        		
+		int number = sc.nextInt();
+		
+		// Check if number is positive integer
+		if(number > 0){
+		    for(int i=1; i<=number; i++){
+		        // Check the entered number is either even or odd
+		        if(i % 2 == 0){
+                    System.out.println(i + " is Even");
+                } 
+				else {
+                    System.out.println(i + " is Odd");
+                }
+		    }	
+		}
+			
+		// Closing the Scanner Stream
+		sc.close();
+         		
+    }
+}
