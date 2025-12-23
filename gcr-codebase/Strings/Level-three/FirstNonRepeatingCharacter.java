6d2846c799dcedb4453cc0651d77c64018205ba9
Dishagujar26 <dishagujar1@gmail.com>
2025-12-23 13:14:10 +0530

    [Disha Gujar] : added find first non-repeating character program file

---
+++ gcr-codebase/Strings/Level-three/FirstNonRepeatingCharacter.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class FirstNonRepeatingCharacter {
+
+    // Method to find first non-repeating character using charAt()
+    public static char findFirstNonRepeatingChar(String text) {
+
+        // Array to store frequency of ASCII characters
+        int[] freq = new int[256];
+
+        // Step 1: Find frequency of each character
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            freq[ch]++;
+        }
+
+        // Step 2: Find first character with frequency 1
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (freq[ch] == 1) {
+                return ch;
+            }
+        }
+
+        // If no non-repeating character found
+        return '\0';
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter text:");
+        String text = sc.nextLine();
+
+        char result = findFirstNonRepeatingChar(text);
+
+        if (result != '\0') {
+            System.out.println("First non-repeating character: " + result);
+        } else {
+            System.out.println("No non-repeating character found");
+        }
+
+        sc.close();
+    }
+}
