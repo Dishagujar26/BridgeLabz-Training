14a326598781eaa56a1780c25c8b7aedeff4eb42
Dishagujar26 <dishagujar1@gmail.com>
2025-12-31 22:59:16 +0530

    [Disha Gujar] : Added Faculty Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/objectmodeling/selfproblems/problem2/Faculty.java
@@ -0,0 +1,14 @@
+package com.bl.objectmodeling.selfproblems.problem2;
+
+public class Faculty {
+	    String name;
+
+	    Faculty(String name) {
+	        this.name = name;
+	    }
+
+	    void showFaculty() {
+	        System.out.println("Faculty: " + name);
+	    }
+
+}
