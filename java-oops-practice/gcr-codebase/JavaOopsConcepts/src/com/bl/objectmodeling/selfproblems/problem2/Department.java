1a8a9b9fb35f7a58516e5e3ca7d014fdcf7de2d0
Dishagujar26 <dishagujar1@gmail.com>
2025-12-31 22:58:52 +0530

    [Disha Gujar] : Added Department Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/objectmodeling/selfproblems/problem2/Department.java
@@ -0,0 +1,14 @@
+package com.bl.objectmodeling.selfproblems.problem2;
+
+public class Department {
+	 String deptName;
+
+	    Department(String deptName) {
+	        this.deptName = deptName;
+	    }
+
+	    void showDepartment() {
+	        System.out.println("Department: " + deptName);
+	    }
+
+}
