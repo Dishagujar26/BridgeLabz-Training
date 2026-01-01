6aa8ff2f01dccf61356abf8fbe87c88ea00e89e3
Dishagujar26 <dishagujar1@gmail.com>
2026-01-01 21:23:56 +0530

    [Disha Gujar] : Added Person Program File

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/inheritance/hybridinheritance/restaurantmanagement/Person.java
@@ -0,0 +1,12 @@
+package com.bl.inheritance.hybridinheritance.restaurantmanagement;
+
+public class Person {
+	String name;
+	int id;
+
+	Person(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+}
