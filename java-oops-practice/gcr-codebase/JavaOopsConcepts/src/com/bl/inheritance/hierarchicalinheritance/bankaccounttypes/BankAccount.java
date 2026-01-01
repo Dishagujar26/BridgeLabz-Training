c43722abcda63b09bbbad035a8b0afacc3b6cb6b
Dishagujar26 <dishagujar1@gmail.com>
2026-01-01 21:14:37 +0530

    [Disha Gujar] : Added Bank Account Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/inheritance/hierarchicalinheritance/bankaccounttypes/BankAccount.java
@@ -0,0 +1,13 @@
+package com.bl.inheritance.hierarchicalinheritance.bankaccounttypes;
+
+public class BankAccount {
+	int accountNumber ;
+	double balance;
+	
+	public BankAccount(int accountNumber, double balance) {
+		super();
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+}
