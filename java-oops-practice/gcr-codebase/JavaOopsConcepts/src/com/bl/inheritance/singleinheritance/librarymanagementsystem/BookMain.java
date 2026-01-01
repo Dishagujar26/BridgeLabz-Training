50d8d86685a95d2c41f56b3e3c38329b00876295
Dishagujar26 <dishagujar1@gmail.com>
2026-01-01 21:38:59 +0530

    [Disha Gujar] : Added Book Main Program File

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/inheritance/singleinheritance/librarymanagementsystem/BookMain.java
@@ -0,0 +1,8 @@
+package com.bl.inheritance.singleinheritance.librarymanagementsystem;
+
+public class BookMain {
+	public static void main(String[] args) {
+		Book book = new Author("A Little Life", 1995, "Haniya Yanagihara", "One of New York's best-seller author!");
+		book.displayInfo();
+	}
+}
