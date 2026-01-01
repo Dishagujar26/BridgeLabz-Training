bc972660fd6ec2d2c669b12d79a5133c41adf15f
Dishagujar26 <dishagujar1@gmail.com>
2026-01-01 21:27:57 +0530

    [Disha Gujar] : Added ElectricVehicle Program File

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/inheritance/hybridinheritance/vehiclemanagementsystem/ElectricVehicle.java
@@ -0,0 +1,11 @@
+package com.bl.inheritance.hybridinheritance.vehiclemanagementsystem;
+
+public class ElectricVehicle extends Vehicle {
+	ElectricVehicle(String model, int maxSpeed) {
+		super(model, maxSpeed);
+	}
+
+	void charge() {
+		System.out.println("Electric vehicle is charging.");
+	}
+}
