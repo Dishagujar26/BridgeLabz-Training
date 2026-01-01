bd2f91cde08d6e218f4798a2a349de66f87f3874
Dishagujar26 <dishagujar1@gmail.com>
2026-01-01 21:19:12 +0530

    [Disha Gujar] : Added Person Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/inheritance/hierarchicalinheritance/schoolsystem/Person.java
@@ -0,0 +1,13 @@
+package com.bl.inheritance.hierarchicalinheritance.schoolsystem;
+
+public class Person {
+
+	String name;
+	int age;
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+}
