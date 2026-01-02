5e37fc790964aa44ac91c3199d0905a6b16d0547
Dishagujar26 <dishagujar1@gmail.com>
2026-01-02 20:11:22 +0530

    [Disha Gujar] : Added LibraryItem Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/encapsulation/libraraysystem/LibraryItem.java
@@ -0,0 +1,30 @@
+package com.bl.encapsulation.libraraysystem;
+
+public abstract class LibraryItem {
+	private int itemId;
+	private String title;
+	private String author;
+	private String borrowerName; // sensitive data
+
+	public LibraryItem(int itemId, String title, String author) {
+		this.itemId = itemId;
+		this.title = title;
+		this.author = author;
+	}
+
+	// Encapsulation
+	protected void setBorrowerName(String name) {
+		this.borrowerName = name;
+	}
+
+	protected String getBorrowerName() {
+		return borrowerName;
+	}
+
+	public abstract int getLoanDuration();
+
+	public void getItemDetails() {
+		System.out.println("ID: " + itemId + ", Title: " + title + ", Author: " + author);
+	}
+
+}
