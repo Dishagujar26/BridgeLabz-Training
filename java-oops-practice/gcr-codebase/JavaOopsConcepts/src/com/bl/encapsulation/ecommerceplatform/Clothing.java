f36f8bc87a5421b9f156bcc08c054fe087024ef9
Dishagujar26 <dishagujar1@gmail.com>
2026-01-02 20:00:07 +0530

    [Disha Gujar] : Added Clothing Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bl/encapsulation/ecommerceplatform/Clothing.java
@@ -0,0 +1,21 @@
+package com.bl.encapsulation.ecommerceplatform;
+
+public class Clothing extends Product implements Taxable {
+
+	public Clothing(int productId, String name, double price) {
+		super(productId, name, price);
+	}
+
+	public double calculateDiscount() {
+		return getPrice() * 0.20; 
+	}
+
+	public double calculateTax() {
+		return getPrice() * 0.05;
+	}
+
+	public String getTaxDetails() {
+		return "5% GST on Clothing";
+	}
+
+}
