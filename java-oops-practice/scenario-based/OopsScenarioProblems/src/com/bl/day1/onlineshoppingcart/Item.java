6df4b4dfe084ac458dd3e75a5e0034420f494884
Dishagujar26 <dishagujar1@gmail.com>
2026-01-03 11:56:48 +0530

    [Disha Gujar] : Added Item Program file

---
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day1/onlineshoppingcart/Item.java
@@ -0,0 +1,13 @@
+package com.bl.day1.onlineshoppingcart;
+
+public class Item {
+
+	String name;
+	double price;
+
+	Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+}
