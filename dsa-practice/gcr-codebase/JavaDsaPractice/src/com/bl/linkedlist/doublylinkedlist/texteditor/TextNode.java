32318889334743812ef1ceedde11e573195ffe74
Dishagujar26 <dishagujar1@gmail.com>
2026-01-03 11:32:50 +0530

    [Disha Gujar]: Added TextEditorNode Program File

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/bl/linkedlist/doublylinkedlist/texteditor/TextNode.java
@@ -0,0 +1,11 @@
+package com.bl.linkedlist.doublylinkedlist.texteditor;
+
+public class TextNode {
+	String content;
+    TextNode prev;
+    TextNode next;
+
+    TextNode(String content) {
+        this.content = content;
+    }
+}
