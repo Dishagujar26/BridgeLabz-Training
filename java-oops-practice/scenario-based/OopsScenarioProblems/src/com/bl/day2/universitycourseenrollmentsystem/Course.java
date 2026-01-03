75d590a04d82bd3e03a424999295a30735a7d457
Dishagujar26 <dishagujar1@gmail.com>
2026-01-03 12:28:39 +0530

    [Disha Gujar] : Added Course Program file

---
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day2/universitycourseenrollmentsystem/Course.java
@@ -0,0 +1,11 @@
+package com.bl.day2.universitycourseenrollmentsystem;
+
+public class Course {
+	int courseId;
+    String courseName;
+
+    Course(int courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+}
