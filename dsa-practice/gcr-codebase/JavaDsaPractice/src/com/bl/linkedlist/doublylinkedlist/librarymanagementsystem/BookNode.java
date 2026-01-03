8e400f72c1ee7ad530704dbecf45ec6aa5a9c857
Dishagujar26 <dishagujar1@gmail.com>
2026-01-03 11:23:29 +0530

    [Disha Gujar]: Added BookNode Program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/bl/linkedlist/doublylinkedlist/librarymanagementsystem/BookNode.java
@@ -0,0 +1,19 @@
+package com.bl.linkedlist.doublylinkedlist.librarymanagementsystem;
+
+public class BookNode {
+	int bookId;
+	String title;
+	String author;
+	String genre;
+	boolean isAvailable;
+	BookNode next;
+	BookNode prev;
+
+	BookNode(int bookId, String title, String author, String genre, boolean isAvailable) {
+		this.bookId = bookId;
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.isAvailable = isAvailable;
+	}
+}
