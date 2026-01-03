e4a27eea8be8eb58d77fc04f2d43438a9d38d39a
Dishagujar26 <dishagujar1@gmail.com>
2026-01-03 12:33:08 +0530

    [Disha Gujar] : Added Postgraduate Program file

---
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day2/universitycourseenrollmentsystem/Postgraduate.java
@@ -0,0 +1,7 @@
+package com.bl.day2.universitycourseenrollmentsystem;
+
+public class Postgraduate extends Student{
+	 Postgraduate(int id, String name, double gpa) {
+	        super(id, name, gpa);
+	    }
+}
