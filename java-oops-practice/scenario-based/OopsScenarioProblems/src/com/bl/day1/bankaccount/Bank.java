b618d405fcd0c10fc6a793cf84e5c1aaae527bce
Dishagujar26 <dishagujar1@gmail.com>
2026-01-04 19:15:32 +0530

    [Disha Gujar] : Modified the code

--- java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day1/bankaccount/Bank.java
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day1/bankaccount/Bank.java
@@ -1,18 +1,43 @@
 package com.bl.day1.bankaccount;
 
+import java.util.Scanner;
+
 public class Bank {
-	public class BankApp {
-	    public static void main(String[] args) {
-	        Account acc1 = new SavingAccount(101, 1000);
-	        Account acc2 = new CurrentAccount(102, 2000);
+	public static void main(String[] args) {
 
-	        acc1.deposit(500);
-	        acc1.calculateInterest();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Savings Account Number: ");
+		int sAccNo = sc.nextInt();
 
-	        acc2.withdraw(300);
+		System.out.print("Enter Initial Savings Balance: ");
+		double sBalance = sc.nextDouble();
 
-	        System.out.println("Savings Balance: " + acc1.getBalance());
-	        System.out.println("Current Balance: " + acc2.getBalance());
-	    }
+		Account acc1 = new SavingAccount(sAccNo, sBalance);
+
+		System.out.print("Enter amount to deposit in Savings: ");
+		double depositAmt = sc.nextDouble();
+		acc1.deposit(depositAmt);
+
+		acc1.calculateInterest();
+
+		// Current Account input
+		System.out.print("\nEnter Current Account Number: ");
+		int cAccNo = sc.nextInt();
+
+		System.out.print("Enter Initial Current Balance: ");
+		double cBalance = sc.nextDouble();
+
+		Account acc2 = new CurrentAccount(cAccNo, cBalance);
+
+		System.out.print("Enter amount to withdraw from Current: ");
+		double withdrawAmt = sc.nextDouble();
+		acc2.withdraw(withdrawAmt);
+
+		// Output
+		System.out.println("\nSavings Balance: " + acc1.getBalance());
+		System.out.println("Current Balance: " + acc2.getBalance());
+
+		sc.close();
 	}
+
 }
