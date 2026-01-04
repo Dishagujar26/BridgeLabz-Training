1b884384127980efceceeecd9dbcc2d416dd52b2
Dishagujar26 <dishagujar1@gmail.com>
2026-01-04 19:20:42 +0530

    [Disha Gujar] : Modified the code

--- java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day1/onlineshoppingcart/DataStructureExample.java
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day1/onlineshoppingcart/DataStructureExample.java
@@ -4,33 +4,70 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class DataStructureExample {
 	public static void main(String[] args) {
 
-		// Shopping Cart
-		List<Item> cart = new ArrayList<>();
-		cart.add(new Item("Laptop", 50000));
-		cart.add(new Item("Mouse", 500));
+		Scanner sc = new Scanner(System.in);
 
-		// Map
+		// list for shopping Cart
+		List<Item> cart = new ArrayList<>();
+		System.out.print("Enter number of items: ");
+		int itemCount = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < itemCount; i++) {
+			System.out.print("Item name: ");
+			String name = sc.nextLine();
+			System.out.print("Item price: ");
+			int price = sc.nextInt();
+			sc.nextLine();
+			cart.add(new Item(name, price));
+		}
+
+		// Map Books by Author
 		Map<String, List<Book>> booksByAuthor = new HashMap<>();
-		booksByAuthor.put("Author1", new ArrayList<>());
-		booksByAuthor.get("Author1").add(new Book("Book A"));
+		System.out.print("Enter author name: ");
+		String author = sc.nextLine();
+
+		booksByAuthor.put(author, new ArrayList<>());
+
+		System.out.print("Enter number of books: ");
+		int bookCount = sc.nextInt();
+		sc.nextLine();
+
+		for (int i = 0; i < bookCount; i++) {
+			System.out.print("Book title: ");
+			booksByAuthor.get(author).add(new Book(sc.nextLine()));
+		}
 
 		// StringBuilder
-		StringBuilder sb = new StringBuilder("Java");
-		sb.append(" Practice");
+		System.out.print("Enter base string: ");
+		StringBuilder sb = new StringBuilder(sc.nextLine());
+		System.out.print("Enter string to append: ");
+		sb.append(sc.nextLine());
 
 		// Frequency using Map
-		String[] words = "java java spring".split(" ");
+		System.out.print("Enter sentence: ");
+		String[] words = sc.nextLine().split(" ");
 		Map<String, Integer> freq = new HashMap<>();
+
 		for (String w : words) {
 			freq.put(w, freq.getOrDefault(w, 0) + 1);
 		}
 
 		// Employee List
 		List<Employee> employees = new ArrayList<>();
-		employees.add(new Employee(1, "Amit"));
+		System.out.print("Enter employee id: ");
+		int id = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Enter employee name: ");
+		String empName = sc.nextLine();
+
+		employees.add(new Employee(id, empName));
+
+		sc.close();
+
 	}
 }
