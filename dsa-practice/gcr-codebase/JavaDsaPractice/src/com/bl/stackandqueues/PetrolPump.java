2c0725fbf359581120b84c8ea177969084042964
Dishagujar26 <dishagujar1@gmail.com>
2026-01-05 11:45:19 +0530

    [Disha Gujar] : Added PetrolPump Program File

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/bl/stackandqueues/PetrolPump.java
@@ -0,0 +1,12 @@
+package com.bl.stackandqueues;
+
+public class PetrolPump {
+	int petrol;
+	int distance;
+
+	PetrolPump(int petrol, int distance) {
+		this.petrol = petrol;
+		this.distance = distance;
+	}
+
+}
