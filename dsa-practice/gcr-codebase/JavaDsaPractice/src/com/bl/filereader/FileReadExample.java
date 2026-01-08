00bb9f189698920b2307d8202486951e62400580
Dishagujar26 <dishagujar1@gmail.com>
2026-01-08 11:36:38 +0530

    [Disha GUjar] : Added FileReadExample Program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/bl/filereader/FileReadExample.java
@@ -0,0 +1,26 @@
+package com.bl.filereader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadExample {
+
+    public static void main(String[] args) {
+
+    	String filePath = "C:/Users/acer/OneDrive/Desktop/Sample.txt";
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            bufferedReader.close(); 
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
