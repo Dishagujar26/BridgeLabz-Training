75fc23bfed159300a0f4142afe0f181c907d5876
Dishagujar26 <dishagujar1@gmail.com>
2026-01-08 11:41:12 +0530

    [Disha GUjar] : Added Search2DMatrix Program

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/bl/binarysearch/Search2DMatrix.java
@@ -0,0 +1,34 @@
+package com.bl.binarysearch;
+
+public class Search2DMatrix {
+	public static boolean searchMatrix(int[][] matrix, int target) {
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+
+		int left = 0, right = rows * cols - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			int r = mid / cols;
+			int c = mid % cols;
+			int midVal = matrix[r][c];
+
+			if (midVal == target)
+				return true;
+			else if (midVal < target)
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 3, 5 }, { 7, 9, 11 }, { 13, 15, 17 } };
+		int target = 9;
+		boolean found = searchMatrix(matrix, target);
+		System.out.println("Target " + target + " found: " + found);
+	}
+
+}
