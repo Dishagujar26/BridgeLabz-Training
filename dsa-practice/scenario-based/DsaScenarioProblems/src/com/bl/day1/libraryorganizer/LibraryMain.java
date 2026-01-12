3e32b0a25b636c1a4621801ad1ce340f35e7dd7c
Dishagujar26 <dishagujar1@gmail.com>
2026-01-12 12:23:27 +0530

    [Disha Gujar] : Added Library Organizer App

---
+++ dsa-practice/scenario-based/DsaScenarioProblems/src/com/bl/day1/libraryorganizer/LibraryMain.java
@@ -0,0 +1,80 @@
+package com.bl.day1.libraryorganizer;
+
+import java.util.Scanner;
+
+public class LibraryMain {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		LibraryShelf shelf = new LibraryShelf();
+
+		Book b1 = new Book("Harry Potter", "J K Rowling");
+		Book b2 = new Book("The Hobbit", "Tolkien");
+		Book b3 = new Book("Clean Code", "Robert Martin");
+		Book b4 = new Book("Brief History of Time", "Stephen Hawking");
+
+		shelf.addInternal("Fantasy", b1);
+		shelf.addInternal("Fantasy", b2);
+		shelf.addInternal("Programming", b3);
+		shelf.addInternal("Science", b4);
+
+		boolean running = true;
+
+		while (running) {
+
+			System.out.println("\nWhat do you want to do?");
+			System.out.println("1. Borrow Book");
+			System.out.println("2. Return/Add Book");
+			System.out.println("3. Exit");
+
+			int choice = sc.nextInt();
+			sc.nextLine(); // clear buffer
+
+			if (choice == 2) { // Return / Add
+				System.out.println("\nHere is the catalog of books genre wise\n");
+				shelf.displayCatalog();
+
+				System.out.println("\nEnter the genre type");
+				String genre = sc.nextLine();
+
+				System.out.println("Enter the name of the book");
+				String name = sc.nextLine();
+
+				System.out.println("Enter the author name");
+				String authorName = sc.nextLine();
+
+				Book b5 = new Book(name, authorName);
+				shelf.add(genre, b5);
+			}
+
+			else if (choice == 1) { // Borrow
+				System.out.println("\nHere is the catalog of books genre wise\n");
+				shelf.displayCatalog();
+
+				System.out.println("\nEnter the genre type");
+				String genre = sc.nextLine();
+
+				System.out.println("Enter the name of the book");
+				String name = sc.nextLine();
+
+				System.out.println("Enter the author name");
+				String authorName = sc.nextLine();
+
+				Book b5 = new Book(name, authorName);
+				shelf.borrow(genre, b5);
+			}
+
+			else if (choice == 3) {
+				running = false;
+				System.out.println("Thank you for using the Library System!");
+			}
+
+			else {
+				System.out.println("Invalid Choice!!");
+			}
+		}
+
+		sc.close();
+	}
+
+}
---
+++ dsa-practice/scenario-based/DsaScenarioProblems/src/com/bl/day1/libraryorganizer/LibraryShelf.java
@@ -0,0 +1,59 @@
+package com.bl.day1.libraryorganizer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LibraryShelf {
+
+	HashMap<String, List<Book>> catalog = new HashMap<>();
+	HashSet<Book> uniqueBooks = new HashSet<>();
+	
+	void addInternal(String genre, Book book) {
+        if (uniqueBooks.contains(book)) return;
+
+        catalog.putIfAbsent(genre, new LinkedList<>());
+        catalog.get(genre).add(book);
+        uniqueBooks.add(book);
+    }
+
+	// adding/returning book to shelf as per genre
+	public void add(String genre, Book book) {
+		if (uniqueBooks.contains(book)) {
+			System.out.println("Sorry, book already exist in shelf!!");
+			return;
+		}
+
+		// check if that genre exist in HashMap if not then create one with empty list
+		catalog.putIfAbsent(genre, new LinkedList<>());
+		// add book to specific genre book list
+		catalog.get(genre).add(book);
+		// add book to unique books
+		uniqueBooks.add(book);
+		System.out.println(book + " with " + genre + " successfully added to the shelf!");
+	}
+
+	// borrow book to shelf as per genre
+	public void borrow(String genre, Book book) {
+		List<Book> list = catalog.get(genre); // get that specific genre list
+		// book needs to be removed from both the list genre list in HashMap and in
+		// unique books list
+
+		if (list != null && list.remove(book)) {
+			uniqueBooks.remove(book);
+			System.out.println(book + " with " + genre + " successfully borrowed from the library shelf!");
+		}
+		else {
+			System.out.println("Sorry, we couldn't find the book!");
+		}
+	}
+	
+	// Display genre wise catalog
+    void displayCatalog() {
+        for (String genre : catalog.keySet()) {
+            System.out.println(genre + " : " + catalog.get(genre));
+        }
+    }
+
+}
