21b72919832ccb16bc570e93c5c3f48e4b45949d
Dishagujar26 <dishagujar1@gmail.com>
2026-01-13 17:02:03 +0530

    [Disa Gujar] : Added library organizer application program files

---
+++ dsa-practice/scenario-based/DsaScenarioProblems/src/com/bl/day3/libraryorganizer/Book.java
@@ -0,0 +1,26 @@
+package com.bl.day3.libraryorganizer;
+
+public class Book {
+	protected String title;
+	protected String authorName;
+
+	public Book(String title, String authorName) {
+		super();
+		this.title = title;
+		this.authorName = authorName;
+	}
+
+	public boolean equals(Object o) {
+		Book b = (Book) o;
+		return title.equals(b.title) && authorName.equals(b.authorName);
+	}
+
+	public int hashCode() {
+		return title.hashCode() + authorName.hashCode();
+	}
+
+	public String toString() {
+		return title + " by " + authorName;
+	}
+
+}
