64160457a9c3f8e97d5b6fb3e93a60d53146b465
Dishagujar26 <dishagujar1@gmail.com>
2026-01-21 11:10:57 +0530

    [Disha Gujar]: Added Contact Files added

---
+++ java-oops-practice/scenario-based/OopsScenarioProblems/src/com/bl/day12/Contact.java
@@ -0,0 +1,45 @@
+package com.bl.day12;
+
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String email;
+	private Address address;
+
+	public Contact(String firstName, String lastName, String phone, String email, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void updateContact(String phone, String email, Address address) {
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+	}
+
+	public String getFullName() {
+		return firstName + " " + lastName;
+	}
+
+	@Override
+	public String toString() {
+		return getFullName() + " | Phone: " + phone + " | Email: " + email + " | Address: " + address;
+	}
+}
