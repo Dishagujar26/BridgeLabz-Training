bd24e3d9074bb686e2ff9bec5b83832336e76d7b
Dishagujar26 <dishagujar1@gmail.com>
2026-01-26 17:49:07 +0530

    [Disha Gujar] : Added deprecated annotation program files

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/bl/annotations/deprecated/Test.java
@@ -0,0 +1,9 @@
+package com.bl.annotations.deprecated;
+
+public class Test {
+	public static void main(String[] args) {
+		LegacyAPI api = new LegacyAPI();
+		api.oldFeature(); // warning
+		api.newFeature();
+	}
+}
