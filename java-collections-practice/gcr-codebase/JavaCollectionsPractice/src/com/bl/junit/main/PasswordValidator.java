dae8348b854a903e6bc6bfd4a0f97edbed306317
Dishagujar26 <dishagujar1@gmail.com>
2026-01-27 14:53:24 +0530

    [Disha Gujar] : Added PasswordValidator Class File

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/bl/junit/main/PasswordValidator.java
@@ -0,0 +1,8 @@
+package com.bl.junit.main;
+
+public class PasswordValidator {
+
+    public boolean isValid(String password) {
+        return password.matches("(?=.*[A-Z])(?=.*\\d).{8,}");
+    }
+}
