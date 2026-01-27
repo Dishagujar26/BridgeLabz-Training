6451aeadd0195a78ef4a062ed659027406bbfc09
Dishagujar26 <dishagujar1@gmail.com>
2026-01-27 14:59:52 +0530

    [Disha Gujar] : Added DatabaseConnection Test File

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/bl/junit/test/DatabaseConnectionTest.java
@@ -0,0 +1,30 @@
+package com.bl.junit.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.bl.junit.main.DatabaseConnection;
+
+public class DatabaseConnectionTest {
+
+    DatabaseConnection db;
+
+    @BeforeEach
+    void setup() {
+        db = new DatabaseConnection();
+        db.connect();
+    }
+
+    @AfterEach
+    void cleanup() {
+        db.disconnect();
+    }
+
+    @Test
+    void testConnectionStatus() {
+        assertTrue(db.isConnected());
+    }
+}
